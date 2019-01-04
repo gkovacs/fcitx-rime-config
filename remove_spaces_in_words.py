1ae06fa5625b67f7569d39258ed143803b30187b
Geza Kovacs <geza0kovacs@gmail.com>
2019-01-03 18:47:51 -0800

    remove spaces from dictionary to fix reverse lookup. related to https://github.com/rime/home/issues/298

---
+++ remove_spaces_in_words.py
@@ -0,0 +1,15 @@
+lines = open('leimaau_jyutping.dict.yaml').readlines()
+output = []
+for x in lines:
+  #x = x.strip()
+  if '\t' not in x:
+    output.append(x)
+  else:
+    tabidx = x.index('\t')
+    first = x[:tabidx]
+    rest = x[tabidx:]
+    rest = rest.replace(' ', '')
+    #rest = rest.replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '')
+    output.append(first + rest)
+for x in output:
+  print(x, end='')
